0ee9d4b1c04fb6153104a12e11f4c0973a8089b1
Sourav Garg <38741296+oravus@users.noreply.github.com>
2025-09-14 15:18:26 +0930

    Set Model path for pixnav

--- libs/pixnav/constants.py
+++ libs/pixnav/constants.py
@@ -10,5 +10,5 @@ EPISODE_PREFIX = "{YOUR_DATASET_DIR}"
 # SAM_ENCODER_VERSION = "vit_h"
 # SAM_CHECKPOINT_PATH = "./checkpoints/sam_vit_h_4b8939.pth"
 # policy checkpoint
-POLICY_CHECKPOINT = "<main_dir>/model_weights/pixelnav_A.ckpt"
+POLICY_CHECKPOINT = "model_weights/pixelnav_A.ckpt"
 
